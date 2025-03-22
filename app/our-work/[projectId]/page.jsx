879777cd9c10dac04e57f250ce798c08eb3fc9a2
miloss98 <stojanovic98m@gmail.com>
2025-03-22 17:28:02 +0100

    changed logic to fetch the data on server

--- app/our-work/[projectId]/page.jsx
+++ app/our-work/[projectId]/page.jsx
@@ -1,6 +1,9 @@
-import SingleProjectClientSide from './clientSide';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export async function generateMetadata({ params }, parent) {
+import { ArrowLeft } from 'react-feather';
+
+export async function generateMetadata({ params }) {
   const { projectId } = await params;
 
   const product = await fetch(
@@ -18,9 +21,79 @@ export async function generateMetadata({ params }, parent) {
 export default async function SingleProjectPage({ params }) {
   const projectId = (await params).projectId;
 
+  const response = await fetch(
+    `https://pixabay.com/api/?key=${process.env.NEXT_PUBLIC_API_KEY}&q=interior+design&image_type=photo&id=${projectId}`,
+  );
+  const data = await response.json();
+  const projectData = data.hits[0];
+
+  if (!projectData) return;
+
   return (
     <div className="mt-20 px-5 md:px-10 lg:px-20">
-      <SingleProjectClientSide projectId={projectId} />
+      <div className="my-20">
+        <div className="w-full flex flex-col lg:flex-row gap-12 pt-12">
+          <div className="w-full lg:w-1/2 flex flex-col gap-8 md:gap-16">
+            <Link
+              href="/our-work"
+              className="flex gap-2 items-center text-xl hover:font-medium"
+            >
+              <ArrowLeft className="w-6 h-6" />
+              Back to projects
+            </Link>
+            <h1 className="text-4xl">Project: {projectData.user}</h1>
+            <div className="flex flex-col gap-4 md:gap-8">
+              <p className="text-xl md:text-2xl lg:text-2xl">
+                Page views: {projectData.views}
+              </p>
+              <p className="text-xl md:text-2xl lg:text-2xl">
+                Downloads: {projectData.downloads}
+              </p>
+              <p className="text-xl md:text-2xl lg:text-2xl">
+                Likes: {projectData.likes}
+              </p>
+
+              <Link
+                href={projectData.pageURL}
+                className="font-bold hover:underline text-xl"
+              >
+                {projectData.pageURL}{' '}
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 w-full lg:w-1/2">
+            <div className="relative aspect-video w-full">
+              <Image
+                src={projectData.webformatURL}
+                alt={projectData.user}
+                fill
+                sizes="100% 100%"
+                className="rounded-xl"
+                priority
+              />
+            </div>
+            <div className="flex items-center gap-4">
+              {projectData.userImageURL && (
+                <div className="relative w-12 aspect-square">
+                  <Image
+                    src={projectData.userImageURL}
+                    alt={projectData.user}
+                    fill
+                    sizes="100% 100%"
+                    className="rounded-full"
+                  />
+                </div>
+              )}
+
+              <h1 className="text-2xl">{projectData.user}</h1>
+            </div>
+            <p className="text-xl flex flex-wrap gap-2">
+              <span className="font-medium"> Tags: </span>
+              {projectData.tags}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- app/our-work/page.jsx
+++ app/our-work/page.jsx
@@ -1,4 +1,7 @@
-import OurWorkClientSide from './clientSide';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const API_URL = `https://pixabay.com/api/?key=${process.env.NEXT_PUBLIC_API_KEY}&q=interior+design&image_type=photo&per_page=12`;
 
 export const metadata = {
   title: 'Our Work | My Website',
@@ -19,10 +22,43 @@ export const metadata = {
   },
 };
 
-export default function OurWork() {
+export default async function OurWork() {
+  const response = await fetch(API_URL);
+  const imagesData = await response.json();
+
+  if (!imagesData) return;
   return (
-    <div>
-      <OurWorkClientSide />
+    <div className="mx-auto mt-20">
+      <div className="py-20 md:px-10 px-5 lg:px-20 flex flex-col gap-12">
+        <h1 className="font-roboto md:text-4xl text-3xl lg:text-5xl text-center">
+          Interior Design Gallery
+        </h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-12">
+          {imagesData.hits.map((image, id) => (
+            <Link
+              key={id}
+              href={`/our-work/${image.id}`}
+              className="flex flex-col gap-2"
+            >
+              <div className="relative overflow-hidden rounded-lg shadow-md aspect-video">
+                <Image
+                  src={image.largeImageURL}
+                  alt={image.tags}
+                  className="w-full h-56 object-cover transition-transform duration-300 hover:scale-105"
+                  fill
+                  priority
+                  sizes="100% 100%"
+                />
+                <span className="absolute inset-0 bg-black bg-opacity-20 opacity-0 hover:opacity-100 flex items-center justify-center text-white text-lg font-bold">
+                  {image.user}
+                </span>
+              </div>
+              <p className="text-center text-bold text-2xl"> {image.user} </p>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
